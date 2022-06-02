2d2544c6d40fd6e64ad4c9dcb60fd1755e901a93
Luis Pedro Coelho <luis@luispedro.org>
2022-06-02 15:12:40 +0200

    TST Use nix for testing
    This allows for caching (including cachix) and is significantly faster

---
+++ release.nix
@@ -0,0 +1,34 @@
+let
+  sources = {
+    haskellNix = builtins.fetchTarball {
+      name = "haskell-nix-snap";
+      url = "https://github.com/input-output-hk/haskell.nix/archive/c6a5afba7e259e9908f9dc56ce711173cda4549b.tar.gz";
+      sha256 = "0g58zvyqd47c1y2w84qbf1x29yp4pfdz2gqligvgn1jlx972i53n";
+    };
+  };
+
+  haskellNix = import sources.haskellNix { };
+
+  # Import nixpkgs and pass the haskell.nix provided nixpkgsArgs
+  pkgs = import
+    # haskell.nix provides access to the nixpkgs pins which are used by our CI,
+    # hence you will be more likely to get cache hits when using these.
+    # But you can also just use your own, e.g. '<nixpkgs>'.
+    haskellNix.sources.nixpkgs-unstable
+    # These arguments passed to nixpkgs, include some patches and also
+    # the haskell.nix functionality itself as an overlay.
+    haskellNix.nixpkgsArgs;
+
+    ignoredPaths = ["docs" ".github"];
+in pkgs.haskell-nix.stackProject {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.lib.cleanSourceWith {
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      name = "interval-int-map";
+      src = ./.;
+    };
+    # ignore paths that change frequently, but do not contribute to the result
+    filter = path: type: let baseName = baseNameOf (toString path); in !(pkgs.lib.elem baseName ignoredPaths);
+  };
+}
+
